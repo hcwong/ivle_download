0b2953e3d620aa5af6a025a3843e58c894ccf7c7
Joshua Wong <joshwonghc@gmail.com>
2018-09-23 14:29:36 +0800

    Trying to parse the xml response

--- ivle_request.py
+++ ivle_request.py
@@ -1,19 +1,28 @@
 import os
 import requests
-
+import xml.etree.ElementTree as et
 from dotenv import load_dotenv
 load_dotenv()
 
 IVLE_KEY = os.getenv("IVLE_KEY")
+IVLE_TOKEN = os.getenv("IVLE_TOKEN")
+
+class IvleRequest:
+  def __init__(self):
+    self.ivle_key = IVLE_KEY
+    self.ivle_token = IVLE_TOKEN
+    self.url = "https://ivle.nus.edu.sg/api/Lapi.svc/"
+
+  def get_files_request(self, module_code, duration):
+    params = {"APIKey": self.ivle_key, "AuthToken": self.ivle_token, "CourseID": module_code, 
+              "Duration": duration, "WorkbinID": "", "TitleOnly": "false"}
+    r = requests.get(self.url + "Workbins", params=params)
+    print(r.content)
+    root = et.fromstring(r.content) 
 
 def main():
-  authentication()
-
-def authentication():
-  auth_url = "https://ivle.nus.edu.sg/api/login/"
-  auth_params = {"apikey": IVLE_KEY, "url": "http://localhost:5000"}
-  r = requests.get(auth_url, params=auth_params)
-  print(r.text)
+  ivle_request = IvleRequest()
+  ivle_request.get_files_request("CS1101S", 0)
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,2 +1,13 @@
-from flask import flask
+from flask import Flask
+from flask import request
+
 app = Flask(__name__)
+
+@app.route("/")
+def login():
+  try: 
+    token_key = request.args.get("token")
+    print(f"This is your token: '{token_key}'. Save it inside the env file")
+    return "Success"
+  except:
+    abort(404)
